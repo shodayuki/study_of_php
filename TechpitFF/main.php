de984a74d8ecc34b6dba54dea55f7e5b3b4103ba
shodayuki <yshoda1227@gmail.com>
2024-01-05 10:27:22 +0900

    add: ダメージを与える処理を実装

--- TechpitFF/main.php
+++ TechpitFF/main.php
@@ -2,3 +2,18 @@
 
 require_once ('./classes/Human.php');
 require_once ('./classes/Enemy.php');
+
+$tiida = new Human();
+$goblin = new Enemy();
+
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
+
+echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+echo "\n";
+
+$tiida->doAttack($goblin);
+echo "\n";
+$goblin->doAttack($tiida);
+echo "\n";
