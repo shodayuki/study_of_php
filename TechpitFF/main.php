fc15da35636160f8f5e4de28be05978537ac74a5
shodayuki <yshoda1227@gmail.com>
2024-01-05 10:45:27 +0900

    add: 繰り返し処理を実装

--- TechpitFF/main.php
+++ TechpitFF/main.php
@@ -9,11 +9,25 @@ $goblin = new Enemy();
 $tiida->name = "ティーダ";
 $goblin->name = "ゴブリン";
 
-echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
-echo "\n";
+$turn = 1;
 
-$tiida->doAttack($goblin);
-echo "\n";
-$goblin->doAttack($tiida);
-echo "\n";
+// どちらかのHPが0になるまで繰り返す
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+
+    echo "*** $turn ターン目 ***\n\n";
+
+    // 現在のHPを表示
+    echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+    echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+    echo "\n";
+
+    // 攻撃
+    $tiida->doAttack($goblin);
+    echo "\n";
+    $goblin->doAttack($tiida);
+    echo "\n";
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
